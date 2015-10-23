79c7c01e45c80ff5eb705e3cd696ce6ff6df6b16
Mora <qiuzhongleiabc@126.com>
2015-10-23 21:49:48 +0800

    添加生成 bash 自动补全的子命令
    仅支持补全参数，并且只能补全全名，不支持别名

--- bin/da-completion.js
+++ bin/da-completion.js
@@ -1,3 +1,9 @@
+var options = require('./da-options');
+
+function keyToCliOption(key) {
+  return key.length === 1 ? '-' + key : '--' + key;
+}
+
 module.exports = function (yargs) {
 
   // 可以用 `da --get-yargs-completions -a -c d` 去调试
@@ -6,11 +12,28 @@ module.exports = function (yargs) {
     // 'current' is the current command being completed.
     // 'argv' is the parsed arguments so far.
     // simply return an array of completions.
-    return [
-      '-ac',
-      'foo',
-      'bar'
-    ];
+
+    var filter = [];
+    var all = [];
+    var matched = false;
+
+    Object.keys(options).forEach(function (key) {
+      var value = argv[key];
+
+      if (/^[a-zA-Z]/.test(key) && value === options[key].default) {
+
+        key = keyToCliOption(key);
+        all.push(key);
+
+        if (key === current) {
+          matched = true;
+        } else if (key.indexOf(current) === 0) {
+          filter.push(key);
+        }
+      }
+    });
+
+    return matched ? filter : all;
   });
 
 
